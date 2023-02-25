1e26f97d06465a4eb922b27d088354110ac50108
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-02-25 13:03:49 +0530

    tagmanager

--- components/Tagmanager.tsx
+++ components/Tagmanager.tsx
@@ -1,20 +1,56 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const Tagmanager: React.FC = () => {
-  useEffect(() => {
-    (function (w, d, s, l, i) {
-      (w as any)[l] = (w as any)[l] || [];
-      (w as any)[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-MHQ847M");
-  }, []);
+// const Tagmanager: React.FC = () => {
+//   useEffect(() => {
+//     (function (w, d, s, l, i) {
+//       (w as any)[l] = (w as any)[l] || [];
+//       (w as any)[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
 
-  return <div></div>;
-};
+//       const f: HTMLElement | null = d.getElementsByTagName(s)[0];
+//       if (f) {
+//         const j: HTMLScriptElement = d.createElement(s);
+//         const dl = l !== "dataLayer" ? "&l=" + l : "";
+//         j.async = true;
+//         j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+//         f.parentNode?.insertBefore(j, f);
+//       }
+//     })(window, document, "script", "dataLayer", "GTM-MHQ847M");
+//   }, []);
 
-export default Tagmanager;
+//   return <div></div>;
+// };
+
+// export default Tagmanager;
+
+import React from 'react'
+
+const Tagmanager = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Tagmanager
+
+
+
+// import React, { useEffect } from "react";
+
+// const Tagmanager: React.FC = () => {
+//   useEffect(() => {
+//     (function (w, d, s, l, i) {
+//       (w as any)[l] = (w as any)[l] || [];
+//       (w as any)[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+//       var f = d.getElementsByTagName(s)[0],
+//         j = d.createElement(s),
+//         dl = l != "dataLayer" ? "&l=" + l : "";
+//       j.async = true;
+//       j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+//       f.parentNode.insertBefore(j, f);
+//     })(window, document, "script", "dataLayer", "GTM-MHQ847M");
+//   }, []);
+
+//   return <div></div>;
+// };
+
+// export default Tagmanager;
